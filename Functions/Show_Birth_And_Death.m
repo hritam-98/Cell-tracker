b54c24e3590737f477109a3ebff331ff1d9a7ac5
Hritam Basak <51586842+hritam-98@users.noreply.github.com>
2021-09-19 04:01:51 +0530

    Add files via upload

---
+++ Functions/Show_Birth_And_Death.m
@@ -0,0 +1,43 @@
+function Show_Birth_And_Death(tracked_images_path, XBorder, YBorder, MaxWindowX, MaxWindowY )
+
+if tracked_images_path(end) ~= filesep
+    tracked_images_path = [tracked_images_path filesep];
+end
+
+% Load Data and Birth And Death
+if ~exist([tracked_images_path 'tracking_workspace.mat'],'file')
+    errordlg(sprintf('Output of tracking not found:\nCheck paths for correctness\nRun segmentation and tracking before plotting output'))
+    return
+end
+
+load([tracked_images_path 'tracking_workspace.mat']);
+if ~exist('birth', 'var'), errordlg('Missing birth required.'); return; end
+if ~exist('death', 'var'), errordlg('Missing death required.'); return; end
+
+% Create figure in case not found
+if ~isempty(findobj('type','figure','name','Birth And Death Window'))
+    BD_fig = findobj('type','figure','name','Birth And Death Window');
+    figure(BD_fig)
+else    
+    BD_fig = figure(...
+    'units', 'pixels',... 
+    'Position', [XBorder+MaxWindowX, YBorder, MaxWindowX*0.3, MaxWindowY], ... 
+    'Name','Birth And Death Window',...
+    'NumberTitle','off',...
+    'Menubar','none',...
+    'Toolbar','none',...
+    'Resize', 'on');
+end
+
+%Scrollable EditBox
+uitable('Parent',BD_fig,...
+    'units','normalized',...
+    'position',[0 0 1 1],...
+    'ColumnName', {' Cell ID ', ' Birth ', ' Death '}, ...
+    'fontweight','bold',...
+    'RowName',[],...
+    'Data',[(1:highest_cell_number)', birth, death]);
+end
+
+
+
