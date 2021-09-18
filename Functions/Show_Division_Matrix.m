cd82d7d61d5ff5be28e4e2a18b6a62ef69f18d9c
Hritam Basak <51586842+hritam-98@users.noreply.github.com>
2021-09-19 04:03:01 +0530

    Add files via upload

---
+++ Functions/Show_Division_Matrix.m
@@ -0,0 +1,43 @@
+function Show_Division_Matrix(tracked_images_path, XBorder, YBorder, MaxWindowX, MaxWindowY )
+
+if tracked_images_path(end) ~= filesep
+    tracked_images_path = [tracked_images_path filesep];
+end
+
+% Load Data and Get confidence index
+if ~exist([tracked_images_path 'tracking_workspace.mat'],'file')
+    errordlg(sprintf('Output of tracking not found:\nCheck paths for correctness\nRun segmentation and tracking before plotting output'))
+    return
+end
+load([tracked_images_path 'tracking_workspace.mat']);
+
+if ~exist('division_matrix', 'var'), errordlg('Missing division_matrix required.'); return; end
+
+% Create figure in case not found
+if ~isempty(findobj('type','figure','name','Division Matrix Window'))
+    DM_fig = findobj('type','figure','name','Division Matrix Window');
+    figure(DM_fig)
+else 
+    DM_fig = figure(...
+    'units', 'pixels',... 
+    'Position', [XBorder+MaxWindowX, YBorder,  MaxWindowX*0.35, MaxWindowY], ...
+    'Name','Division Matrix Window',...
+    'NumberTitle','off',...
+    'Menubar','none',...
+    'Toolbar','none',...
+    'Resize', 'on');
+end
+
+%Scrollable EditBox
+uitable('Parent',DM_fig,...
+    'units','normalized',...
+    'position',[0 0 1 1],...
+    'ColumnName', {' Mother ID ', ' Daughter1 ID ', ' Daughter2ID '}, ...
+    'fontweight','bold', ...
+    'RowName',[],...
+    'Data',[(1:highest_cell_number)', division_matrix]);
+end
+
+
+
+
